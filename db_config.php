106e30eb4e6440a7a94907a21a6376e64a58904f
sharif198711 <148708820+sharif198711@users.noreply.github.com>
2026-07-08 13:05:58 +0200

    chore: update database host and rebuild assets

--- db_config.php
+++ db_config.php
@@ -1,6 +1,6 @@
 <?php
 // تم توليد هذا الملف تلقائياً بواسطة معالج التثبيت وإعدادات قاعدة البيانات الخاصة بك
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 define('DB_NAME', 'u948332049_SharifVxZ');
 define('DB_USER', 'u948332049_Lulia');
 define('DB_PASS', 'venus123123V!');
